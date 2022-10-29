8ca4848824adba7044515c9f868af14f18dedc1c
Ahmed Khaled <Ahmedkhaled.m24@gmail.com>
2022-10-29 19:08:11 +0200

    feat: update server.js file
    add connectMongo function to connect the database before the server
    starts
    import all models files to run before the server starts

--- server/Server.js
+++ server/Server.js
@@ -1,15 +1,19 @@
 import dotenv from 'dotenv'
 dotenv.config();
+import './services/passport.js'
+import './models/client/client.schema.js'
+import './models/admin/admin.schema.js'
+import './models/log/log.schema.js'
 
 import {createServer} from 'http'
 import {api} from './api.js'
+import {connectMongo} from './services/mongo.js'
 
 const server = createServer(api);
 
 (async function () {
-    // Set up database and any other service 
-
+    await connectMongo();
     server.listen(process.env.PORT, ()=>{
-        console.log(`server is listening on port ${process.env.PORT}`);
+        console.log(`Server is listening on port ${process.env.PORT}...`);
     })
 })();
