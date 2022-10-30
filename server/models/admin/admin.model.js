6df544c7b03029674543295597a72d0a69614cc3
Ahmed Khaled <Ahmedkhaled.m24@gmail.com>
2022-10-30 13:02:47 +0200

    feat: add new functions to admin model
    add functions to activate admin, promote admin to super admin,
    degrade admin from super to regular admin

--- server/models/admin/admin.model.js
+++ server/models/admin/admin.model.js
@@ -2,20 +2,40 @@ import mongoose from 'mongoose'
 
 const Admin = mongoose.model('Admin');
 
+async function db_getAllAdmins(){
+    return await Admin.find({});
+}
+
 async function db_addNewAdmin(admin){
     return await Admin.create(admin);
+}
+
+async function db_activateAdmin(id){
+    return await Admin.updateOne({_id: id}, {active: true});
 }
 
 async function db_deactivateAdmin(id){
     return await Admin.updateOne({_id: id}, {active: false});
 }
 
-async function db_getAllAdmins(){
-    return await Admin.find();
+async function db_getAdminById(id){
+    return await Admin.findOne({_id: id});
+}
+
+async function db_promoteAdmin(id){
+    return await Admin.updateOne({_id: id}, {super: true});
+}
+
+async function db_degradeAdmin(id){
+    return await Admin.updateOne({_id: id}, {super: false});
 }
 
 export {
     db_addNewAdmin,
     db_deactivateAdmin,
     db_getAllAdmins,
+    db_getAdminById,
+    db_activateAdmin,
+    db_promoteAdmin,
+    db_degradeAdmin
 }
