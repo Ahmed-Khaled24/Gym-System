95acd967b90ea1d4ae1add47d5a6e6e9ce249284
Ahmed Khaled <Ahmedkhaled.m24@gmail.com>
2022-10-31 12:36:59 +0200

    feat: add cookies and dotenv to api.js

--- server/api.js
+++ server/api.js
@@ -1,13 +1,23 @@
+import dotenv from 'dotenv'
+dotenv.config();
 import express from 'express'
 import passport from 'passport'
+import cookie from 'cookie-session';
 import clientRouter from './routers/client.router.js';
 import adminRouter from './routers/admin.router.js'
 
 const api = express();
 api.use(express.json());
+api.use(cookie({
+    name: 'GYM',
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 30,   
+    }
+}));
 api.use(passport.initialize());
-// api.use(passport.session());
+api.use(passport.session());
 api.use('/client', clientRouter);
 api.use('/admin', adminRouter);
 
-export {api};
+export default api;
