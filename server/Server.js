57320cd426b53714eafcca8b4207dffdb9092d38
Ahmed Khaled <Ahmedkhaled.m24@gmail.com>
2022-10-31 12:32:22 +0200

    refactor: server/server.js
    remove importing dotenv and move it to server/api.js
    refactor the start function to startServer function

--- server/Server.js
+++ server/Server.js
@@ -1,19 +1,15 @@
-import dotenv from 'dotenv'
-dotenv.config();
-import './services/passport.js'
 import './models/client/client.schema.js'
 import './models/admin/admin.schema.js'
 import './models/log/log.schema.js'
-
 import {createServer} from 'http'
-import {api} from './api.js'
+import api from './api.js'
 import {connectMongo} from './services/mongo.js'
 
-const server = createServer(api);
-
-(async function () {
+async function startServer() {
+    const server = createServer(api);
     await connectMongo();
     server.listen(process.env.PORT, ()=>{
         console.log(`Server is listening on port ${process.env.PORT}...`);
     })
-})();
+};
+startServer();
