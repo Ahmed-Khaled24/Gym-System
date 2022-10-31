e45aa9490cecb5bd034c5f3e2091de8d8191a24c
Ahmed Khaled <Ahmedkhaled.m24@gmail.com>
2022-10-31 14:22:28 +0200

    feat: restrict access and add logout

--- server/routers/admin.router.js
+++ server/routers/admin.router.js
@@ -1,21 +1,20 @@
 import {Router} from 'express'
 import * as adminController from '../controllers/admin.controller.js'
 import passport from 'passport'
+import checkLoggedIn from '../middlewares/checkLoggedIn.js';
+import checkSuper from '../middlewares/checkSuper.js'
 
 const adminRouter = Router();
 
-adminRouter.get('/all', adminController.getAllAdmins);
-adminRouter.post('/add', adminController.addNewAdmin);
-adminRouter.post('/activate', adminController.activateAdmin);
-adminRouter.post('/deactivate', adminController.deactivateAdmin);
-adminRouter.post('/promote', adminController.promoteAdmin);
-adminRouter.post('/degrade', adminController.degradeAdmin);
+adminRouter.get('/all', checkLoggedIn, checkSuper, adminController.getAllAdmins);
+adminRouter.post('/add', checkLoggedIn, checkSuper, adminController.addNewAdmin);
+adminRouter.post('/activate', checkLoggedIn, checkSuper, adminController.activateAdmin);
+adminRouter.post('/deactivate', checkLoggedIn, checkSuper, adminController.deactivateAdmin);
+adminRouter.post('/promote', checkLoggedIn, checkSuper, adminController.promoteAdmin);
+adminRouter.post('/degrade', checkLoggedIn, checkSuper, adminController.degradeAdmin);
 adminRouter.post('/login', passport.authenticate('local', {successRedirect: '/admin/login/success', failureRedirect: '/admin/login/failure'}));
-adminRouter.get('/login/success', (req, res) => { 
-    return res.status(200).json({status: 'authenticated'}) 
-});
-adminRouter.get('/login/failure', (req, res) => { 
-    return res.status(401).json({status: 'unauthenticated'}) 
-});
+adminRouter.get('/login/success', (req, res) => { return res.status(200).json({status: 'authenticated'}) });
+adminRouter.get('/login/failure', (req, res) => { return res.status(401).json({status: 'unauthenticated'}) });
+adminRouter.delete('/logout', adminController.logout)
 
 export default adminRouter;
--- server/routers/client.router.js
+++ server/routers/client.router.js
@@ -1,13 +1,14 @@
 import {Router} from 'express'
 import * as clientController from '../controllers/client.controller.js'
-
+import checkLoggedIn from '../middlewares/checkLoggedIn.js';
+import checkActive from '../middlewares/checkActive.js'
 const clientRouter = Router();
 
-clientRouter.get('/all', clientController.getAllClients);
-clientRouter.get('/:id', clientController.getClientLogs);
-clientRouter.post('/add', clientController.addNewClient);
-clientRouter.post('/renew', clientController.updateRenewalDate);
-clientRouter.post('/freeze', clientController.freezeClient);
-clientRouter.post('/unfreeze', clientController.unfreezeClient);
+clientRouter.get('/all', checkLoggedIn, checkActive, clientController.getAllClients);
+clientRouter.get('/:id', checkLoggedIn, checkActive, clientController.getClientLogs);
+clientRouter.post('/add', checkLoggedIn, checkActive, clientController.addNewClient);
+clientRouter.post('/renew', checkLoggedIn, checkActive, clientController.updateRenewalDate);
+clientRouter.post('/freeze', checkLoggedIn, checkActive, clientController.freezeClient);
+clientRouter.post('/unfreeze', checkLoggedIn, checkActive, clientController.unfreezeClient);
 
 export default clientRouter;
